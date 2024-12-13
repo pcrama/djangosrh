bada198760ef752c22b902400565662900b1ecdb
Philippe Crama <pcfeb0009@gmx.com>
2024-12-13 22:59:04 +0100

    First view... following the tutorial

--- djangosrh/ital/views.py
+++ djangosrh/ital/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<html><head><title>Title</title></head><body>Hello, world. You're at the polls index.</body></html>")
