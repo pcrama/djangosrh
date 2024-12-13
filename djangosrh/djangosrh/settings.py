6f5d25bb5c03560e434ffef6c0379f635a3aab6c
Philippe Crama <pcfeb0009@gmx.com>
2024-12-13 22:34:01 +0100

    Add a django app: ital

--- djangosrh/djangosrh/settings.py
+++ djangosrh/djangosrh/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cga^!j+*dz9z6s@%%7ab0niu+92)efsxm^&@5=o+-tgv#r$6z2'
+SECRET_KEY = getenv('DJANGO_SECRET_KEY', 'django-insecure-cga^!j+*dz9z6s@%%7ab0niu+92)efsxm^&@5=o+-tgv#r$6z2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
