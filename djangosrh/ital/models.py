681e8e131015231834526219f1adb9a8cf31b36e
Philippe Crama <pcfeb0009@gmx.com>
2024-12-22 07:56:28 +0000

    More model tweaks

--- djangosrh/ital/models.py
+++ djangosrh/ital/models.py
@@ -1,6 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from enum import Enum
+
+
+# Event:
+# 1."souper italien"@2024-03-29
+# Item:
+# 1. Tomate Mozza; starter
+# 2. Croquettes; starter
+# 3. Bolo; main
+# 4. Scampis; main
+# 5. Vegetarian; main
+# 6. Tiramisu; dessert
+# 7. Glace; dessert
+# Choice
+# 1. Tomate Mozza (single); 9EUR; event_id=1; item_ids=[1]
+# 2. Croquettes (single); 8EUR; event_id=1; item_ids=[2]
+# 3. Bolo (single); 12EUR; event_id=1; item_ids=[3]
+# 4. Tiramisu (single); 6EUR; event_id=1; item_ids=[6]
+# 5. Child menu; 20EUR; event_id=1; item_ids=[3, 5, 6, 7]
+# 6. Bolo menu; 22EUR; event_id=1; item_ids=[1, 2, 3, 6]
+# 7. Scampi menu; 27EUR; event_id=1; item_ids=[1, 2, 4, 6]
+# 8. Anything goes menu; 25EUR; event_id=1; item_ids=[1, 2, 3, 4, 5, 6, 7]
+# Reservation
+# 1. Mr Dupont; dupont@yopmail.fr; [(choice_id_1,[(1,2)]),(choice_id_5,[(3,0),(5,3),(6,1),(7,2)])]; 78EUR
+#        reservation_id=1; choice_id=1; item_id=1; count=2
+#        reservation_id=1; choice_id=5; item_id=3; count=0 (optional row?)
+#        reservation_id=1; choice_id=5; item_id=5; count=3
+#        reservation_id=1; choice_id=5; item_id=6; count=1
+#        reservation_id=1; choice_id=5; item_id=7; count=2
+
+class Civility(models.Choices):
+    man = "Mr"
+    woman = "Mme"
+    __empty__ = ""
 
 
 class Event(models.Model):
@@ -31,12 +64,26 @@ class Item(models.Model):
         return self.display_text
 
 
+class ReservationItemCount(models.Model):
+    count = models.IntegerField()
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['choice_id', 'item_id', 'reservation_id'], name='reservation*item*choice'),
+            # TODO: constrain all choices to be for the same event
+        ]
+
+
 class Reservation(models.Model):
+    civility = models.CharField(max_length=20, choices=Civility)
     last_name = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    accepts_rpgd_reuse = models.BooleanField()
-    attends = models.ForeignKey(Event, on_delete=models.CASCADE)
+    accepts_rgpd_reuse = models.BooleanField()
+    total_due_in_cents = models.IntegerField()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.email})"
+        fullname = " ".join(x for x in (self.civility, self.first_name, self.last_name) if x and x.strip())
+        return f"{fullname} ({self.email})"
